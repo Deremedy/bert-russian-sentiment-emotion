6ed841a9b540b426df3e205431a55e30b2b029f4
Daniels Buls <daniels.buls@protonmail.com>
2024-05-04 12:14:28 +0300

    Refactor configs. Add config for ru_go_emotions_ekman.

--- src/trainer/train.py
+++ src/trainer/train.py
@@ -30,26 +30,37 @@ def train(
 
     for epoch in tq:
         model.train()
-        # Assume train_epoch function handles training logic
+        # Train the model
         train_y_true, train_y_pred, train_loss = train_epoch(
             model, train_dataloader, optimizer, problem_type
         )
 
         model.eval()
-        # Assume predict function handles validation logic
+        # Validate the model
         val_y_true, val_y_pred, val_loss = predict(model, val_dataloader, problem_type)
 
-        # Optionally include calculate_metrics if needed for your application
+        # Update tqdm description with losses
         tq.set_description(f"train_loss: {train_loss:.4f}, val_loss: {val_loss:.4f}")
 
         # Save model and tokenizer using Hugging Face's `save_pretrained`
+        filename = f"{model_name}-{dataset_name}-epoch_{epoch + 1}-val_loss_{val_loss:.4f}"
         model_directory = os.path.join(
             checkpoint_path,
-            f"{model_name}-{dataset_name}-epoch_{epoch + 1}-val_loss_{val_loss:.4f}"
+            filename
         )
         os.makedirs(model_directory, exist_ok=True)
         model.save_pretrained(model_directory)
         tokenizer.save_pretrained(model_directory)
+
+        # Evaluate the model
+        test_y_true, test_y_pred, test_loss = predict(model, test_dataloader, problem_type)
+        report_dict = calculate_metrics(test_y_true, test_y_pred, labels, problem_type)
+
+        # Save evaluation results to the same directory as the model
+        df = pd.DataFrame(report_dict).round(2)
+        print(df)
+        evaluation_filepath = os.path.join(model_directory, "_evaluation_results.csv")
+        df.to_csv(evaluation_filepath)
 
     # Evaluate the model after all epochs are completed
     df = eval(model, test_dataloader, labels, problem_type)
